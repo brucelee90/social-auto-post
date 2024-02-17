640a1d9ec4fdb6e4086ec678aa60b9483034ac86
brucelee90 <leeklopfers@gmx.de>
2024-02-17 12:04:57 +0100

    show all product images

--- app/routes/app.publish_media.tsx
+++ app/routes/app.publish_media.tsx
@@ -1,14 +1,17 @@
 
 
-import type { ActionFunctionArgs} from '@remix-run/node';
+import type { ActionFunctionArgs, LoaderFunctionArgs} from '@remix-run/node';
 // import { redirect } from '@remix-run/node';
-import { Form, useActionData } from '@remix-run/react';
+import { Form, useActionData, useLoaderData } from '@remix-run/react';
+// import { request } from 'axios';
 import { ContainerField, GetContainerRequest, PostPagePhotoMediaRequest, PostPublishMediaRequest } from 'instagram-graph-api'
 import React from 'react'
+import { authenticate } from '~/shopify.server';
 // import { redirect } from 'react-router';
 // import { redirect } from 'react-router';
 
 interface Props {}
+
 
 export async function publishMedia() {
 
@@ -29,6 +32,27 @@ export async function publishMedia() {
     }
 };
 
+export async function loader({request} : LoaderFunctionArgs ) {
+  const {admin} = await authenticate.admin(request);
+  const res = await admin.graphql(
+    `
+      query products {
+        products (first:50, query:"status:active AND published_status:published") {
+        nodes{
+          description
+          featuredImage{
+            url
+          }
+        }
+      }
+    }
+    `
+  )
+
+  return res.json();
+
+}
+
 export const action = async ({
     request,
   }: ActionFunctionArgs) => {
@@ -43,13 +67,34 @@ export const action = async ({
 export default function PublishMedia(props: Props) {
     const {} = props
 
+
     const actionData = useActionData<typeof action>();
     console.log(actionData);
-    
 
+    const data = useLoaderData<typeof loader>();
+    // const edges : [{}] = {...loaderData.data.products.edges}
+    
+    console.log('loaderData', [...data.data.products.nodes] );
+
+    const productsArray = [...data.data.products.nodes]
+
+    productsArray.map(e => {
+      console.log('Product description', e.description);
+      console.log('Product image url', e.featuredImage.url);
+    })
+    
 
     return (
         <Form method="post">
+
+          {
+            productsArray.map((e, key) => {return (
+              <div key={key}>
+                <img alt='img' width={"500px"} src={e.featuredImage.url}/>
+                <p>{e.description}</p>
+              </div>
+            )})
+          }
         
           <button type="submit">PUBLISH MEDIA</button>
 
