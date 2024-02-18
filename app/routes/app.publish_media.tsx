331b9fa139c051df24238a8f74625ff5e5b8b5d8
brucelee90 <leeklopfers@gmx.de>
2024-02-18 20:28:10 +0100

    adds description post

--- app/routes/app.publish_media.tsx
+++ app/routes/app.publish_media.tsx
@@ -1,34 +1,27 @@
 
+import React from 'react'
 
 import type { ActionFunctionArgs, LoaderFunctionArgs} from '@remix-run/node';
-// import { redirect } from '@remix-run/node';
 import { Form, useActionData, useLoaderData } from '@remix-run/react';
-// import { request } from 'axios';
-import { ContainerField, GetContainerRequest, PostPagePhotoMediaRequest, PostPublishMediaRequest } from 'instagram-graph-api'
-import React from 'react'
+import { PostPagePhotoMediaRequest, PostPublishMediaRequest } from 'instagram-graph-api'
 import { authenticate } from '~/shopify.server';
-// import { redirect } from 'react-router';
-// import { redirect } from 'react-router';
-
-interface Props {}
 
 
-export async function publishMedia() {
+
+export async function publishMedia(featuredImageUrl : string, caption : string) {
 
     if (process.env.ACCESS_TOKEN && process.env.PAGE_ID != undefined) {
 
-        let req : PostPagePhotoMediaRequest = new PostPagePhotoMediaRequest(process.env.ACCESS_TOKEN, process.env.PAGE_ID, "https://sneakernews.com/wp-content/uploads/2020/02/off-white-air-jordan-5-virgil-abloh-nike-snkrs-1.jpg")
-        
-        let containerId = await req.execute();
-        console.log('containerId', containerId.getData().id);
+        let pagePhotoMediaRequest : PostPagePhotoMediaRequest = new PostPagePhotoMediaRequest(process.env.ACCESS_TOKEN, process.env.PAGE_ID, featuredImageUrl, caption)
+        let containerId = await pagePhotoMediaRequest.execute();
+        // console.log('containerId', containerId.getData().id)
 
-        let containerReq = new GetContainerRequest(process.env.ACCESS_TOKEN, containerId.getData().id, ContainerField.STATUS_CODE)
-        let statusCode = await containerReq.execute()
-        console.log('status Code:', statusCode.getContainerStatusCode());
+        // let containerReq = new GetContainerRequest(process.env.ACCESS_TOKEN, containerId.getData().id, ContainerField.STATUS_CODE)
+        // let statusCode = await containerReq.execute()
+        // console.log('status Code:', statusCode.getContainerStatusCode());
 
-
-        let pub : PostPublishMediaRequest = new PostPublishMediaRequest(process.env.ACCESS_TOKEN, process.env.PAGE_ID, containerId.getData().id);
-        pub.execute().then(res => console.log('res:', res))
+        let publishMediaRequest : PostPublishMediaRequest = new PostPublishMediaRequest(process.env.ACCESS_TOKEN, process.env.PAGE_ID, containerId.getData().id);
+        publishMediaRequest.execute().then(res => console.log('res:', res))
     }
 };
 
@@ -40,6 +33,8 @@ export async function loader({request} : LoaderFunctionArgs ) {
         products (first:50, query:"status:active AND published_status:published") {
         nodes{
           description
+          title
+          id
           featuredImage{
             url
           }
@@ -48,60 +43,67 @@ export async function loader({request} : LoaderFunctionArgs ) {
     }
     `
   )
-
   return res.json();
-
 }
 
 export const action = async ({
     request,
   }: ActionFunctionArgs) => {
-    const formData = await request.formData();
-    console.log('formData:', formData.getAll('name'));
-
-    publishMedia()
     
+    const formData = await request.formData();
+    const featuredImageUrl = formData.get('featuredImageUrl') as string
+    const customUserDescription = formData.get('customUserDescription') as string
+    const selectedProductDescription = formData.get('selectedProductDescription') as string
+
+    let postDescription = customUserDescription
+    if (!postDescription?.length) {
+      postDescription = selectedProductDescription
+    }
+
+    publishMedia(featuredImageUrl, postDescription)
     return 'PUBLISHED IMAGE SUCCESFULLY !';
-  };
+};
+
+interface Props {}
 
 export default function PublishMedia(props: Props) {
-    const {} = props
-
-
+    const [textareaInput, setTextareaInput] = React.useState(""); 
+    const [selectedProductId, setSelectedProductId] = React.useState(""); 
+    
     const actionData = useActionData<typeof action>();
-    console.log(actionData);
-
     const data = useLoaderData<typeof loader>();
-    // const edges : [{}] = {...loaderData.data.products.edges}
-    
-    console.log('loaderData', [...data.data.products.nodes] );
-
     const productsArray = [...data.data.products.nodes]
-
-    productsArray.map(e => {
-      console.log('Product description', e.description);
-      console.log('Product image url', e.featuredImage.url);
-    })
     
+    const handleChange = (e :any) => {
+      setSelectedProductId(e.target.id);
+    }
+
+    const handleTextAreaInput = (e : any) => {
+       setTextareaInput(e.target.value);
+    }
+
+    const selectedProductDescription = productsArray.find(obj => obj.id === selectedProductId)?.description 
+    const isCustomDescription = true
 
     return (
         <Form method="post">
 
           {
             productsArray.map((e, key) => {return (
-              <div key={key}>
-                <img alt='img' width={"500px"} src={e.featuredImage.url}/>
-                <p>{e.description}</p>
+              <div key={key} >
+                <input type="radio" id={e.id} name="featuredImageUrl" value={e.featuredImage.url} onChange={handleChange}/>
+                <img alt='img' width={"150px"} src={e.featuredImage.url}/> 
+                <label htmlFor="featuredImageUrl">{e.title}</label> 
+                <div style={{width: "30rem", background:"#ccc"}}>{e.description}</div>
               </div>
             )})
           }
-        
-          <button type="submit">PUBLISH MEDIA</button>
 
-          {actionData && (
-            <div>{actionData}</div>
-          )}
-        
+          {isCustomDescription && <textarea rows={4} name="customUserDescription" onInput={handleTextAreaInput} value={textareaInput} cols={50} />}
+          <input type='hidden' name='selectedProductDescription' value={selectedProductDescription} />
+
+          <button type="submit">PUBLISH MEDIA</button>
+          {actionData && (<div>{actionData}</div>)}
       </Form>
     )
 }
