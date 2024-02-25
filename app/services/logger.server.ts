6a7fa564ecb401669a5b0d2d3783bd8286d5b064
brucelee90 <leeklopfers@gmx.de>
2024-02-25 19:05:18 +0100

    Adds winston logger to project

---
+++ app/services/logger.server.ts
@@ -0,0 +1,32 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+
+
+export const logger = createLogger({
+    format: format.combine(
+        format.timestamp(),
+        format.prettyPrint(),
+        format.colorize()
+    ),
+
+    transports: [
+        new transports.File({
+            filename: './app/logs/error.log',
+            maxsize: 300,
+            level: 'error'
+        }),
+        new transports.File({
+            filename: './app/logs/warning.log',
+            maxsize: 300,
+            level: 'warn'
+        }),
+    ],
+    exceptionHandlers: [
+        new transports.File({
+            filename: './app/logs/exceptions.log',
+            maxsize: 300,
+        })
+    ]
+
+});
