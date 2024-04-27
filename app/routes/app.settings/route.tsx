c8116d0156b98a503db71193dd299e4cf30bf7c4
brucelee90 <leeklopfers@gmx.de>
2024-04-28 00:37:30 +0200

    show success message

--- app/routes/app.settings/route.tsx
+++ app/routes/app.settings/route.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import settingsService, { getSettings } from '../../services/SettingsService.server';
-import { Form, json, useActionData, useLoaderData } from '@remix-run/react';
+import {
+    Form,
+    json,
+    useActionData,
+    useFetcher,
+    useFetchers,
+    useLoaderData,
+    useSubmit
+} from '@remix-run/react';
 import { authenticate } from '~/shopify.server';
 import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { Text } from '@shopify/polaris';
-const logger = require('winston');
+
+interface ApiResponse {
+    success: boolean;
+    message: string;
+}
 
 function createApiResponse(success: boolean, message: string) {
-    return { success, message };
+    return { success, message } as ApiResponse;
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -17,9 +29,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
     try {
         shopSettings = await getSettings(shop);
     } catch (error) {
-        logger.error(`Failed to get settings for shop ${shop}: ${(error as Error).message}`, {
-            error
-        });
+        console.log('error while getting settings');
     }
 
     return json({
@@ -44,9 +54,7 @@ export async function action({ request }: ActionFunctionArgs) {
         );
         return createApiResponse(true, 'Settings saved successfully.');
     } catch (error) {
-        logger.error(`Failed to save settings for shop ${shop}: ${(error as Error).message}`, {
-            error
-        });
+        console.log('error while saving setting');
         return createApiResponse(false, 'Failed to save settings.');
     }
 }
@@ -57,6 +65,7 @@ export default function Settings(props: Props) {
     const {} = props;
     const { customPlaceholder } = useLoaderData<typeof loader>();
     const actionData = useActionData<typeof action>();
+    const fetcher = useFetcher({ key: 'add-to-bag' });
 
     return (
         <div>
@@ -64,28 +73,12 @@ export default function Settings(props: Props) {
                 Settings
             </Text>
 
-            {customPlaceholder?.map((e, key) => (
+            {customPlaceholder?.map((customPlaceholderElement, key) => (
                 <div
                     key={key}
                     style={{ display: 'flex', borderBottom: '1px solid black', paddingTop: '1rem' }}
                 >
-                    <Form method="post">
-                        <input
-                            type="hidden"
-                            name="custom_placeholder_name"
-                            value={e.customPlaceholderId}
-                        />
-                        <label htmlFor="custom_placeholder_content" style={{ width: '25%' }}>
-                            {e.customPlaceholderId}:
-                        </label>
-                        <div>
-                            <textarea
-                                name="custom_placeholder_content"
-                                defaultValue={e.customPlaceholderContent}
-                            />
-                            <button type="submit">Update Text Block</button>
-                        </div>
-                    </Form>
+                    <PlaceholderForm placeholder={customPlaceholderElement} />
                 </div>
             ))}
             <Form method="post" style={{ paddingTop: '1rem' }}>
@@ -99,6 +92,51 @@ export default function Settings(props: Props) {
                 </div>
                 <button>Add new Text block</button>
             </Form>
+        </div>
+    );
+}
+
+interface Props {
+    placeholder: {
+        customPlaceholderId: string;
+        customPlaceholderContent: string;
+    };
+}
+
+function PlaceholderForm(props: Props) {
+    const { placeholder } = props;
+    const fetcher = useFetcher({ key: placeholder.customPlaceholderId });
+
+    let message;
+
+    if ((fetcher.data as ApiResponse)?.message) {
+        message = (fetcher.data as ApiResponse).message;
+    }
+
+    return (
+        <div style={{ display: 'flex', borderBottom: '1px solid black', paddingTop: '1rem' }}>
+            <fetcher.Form method="post" key={placeholder.customPlaceholderId}>
+                <input
+                    type="hidden"
+                    name="custom_placeholder_name"
+                    value={placeholder.customPlaceholderId}
+                />
+                <label
+                    htmlFor={`custom_placeholder_content_${placeholder.customPlaceholderId}`}
+                    style={{ width: '25%' }}
+                >
+                    {placeholder.customPlaceholderId}:
+                </label>
+                <div>
+                    <textarea
+                        id={`custom_placeholder_content_${placeholder.customPlaceholderId}`}
+                        name="custom_placeholder_content"
+                        defaultValue={placeholder.customPlaceholderContent}
+                    />
+                    <button type="submit">Update Text Block</button>
+                </div>
+                {message !== undefined && <div>{message}</div>}
+            </fetcher.Form>
         </div>
     );
 }
